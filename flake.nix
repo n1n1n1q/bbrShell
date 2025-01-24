2ab1b67f5d876e37bbb77568380a89918a9ee35b
Oleh Basystyi <basystyo@gmail.com>
2025-01-24 13:13:04 +0200

    fix flake

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     ags.url = "github:aylur/ags";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs, ags }: let
+  outputs = { self, nixpkgs, ags, home-manager }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in rec {
@@ -26,9 +27,14 @@
       ];
     };
 
+    # NixOS module
     nixosModules = {
       programs = import ./nixos/modules.nix;
     };
-    homeManagerModules = import ./nixos/modules.nix;
+    homeManagerModules.bbrShell = import ./nixos/modules.nix;
+    homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
+      inherit system pkgs;
+      modules = [ self.homeManagerModules.bbrShell ];
+    };
   };
 }
