1a0c5bc48330f8fc259c4ccb8716bc1131e45d82
Oleh Basystyi <basystyo@gmail.com>
2025-02-04 13:27:29 +0200

    notifications and OSD

--- app.ts
+++ app.ts
@@ -3,28 +3,21 @@ import style from "./style.scss"
 import Bar from "./widgets/Bar"
 import AppLauncher from "./widgets/AppLauncher"
 import Dashboard from "./widgets/Dashboard"
+import OSD from "./widgets/OSD"
+import NotificationPopups from "./widgets/notifications/NotificationPopup"
 import { restart } from "./utils/utils"
-import { toggleWindow } from "./utils/utils"
 
 App.start({
     css: style,
-    requestHandler(request: string, res: (response: any) => void) {
-        if (request == "launcher") {
-            toggleWindow("launcher");
-            res("launcher toggled");
-        }
-        res("unknown command")
-    },
     main() {
         const monitor = App.get_monitors()[0];
         Dashboard(monitor).hide();
         AppLauncher(monitor).hide();
-
+        App.get_monitors().map(OSD);
+        App.get_monitors().map(NotificationPopups);
         for (const gdkmonitor of App.get_monitors()) {
             Bar(gdkmonitor);
-            print("A");
         }
-        print("B");
         App.connect("monitor-added", (_, gdkmonitor) => {
             restart();
         })
